7690fc4b8b40534c5bc92f0ebef99210485d64e7
Luis Copete <alonsocopete@outlook.es>
2024-02-28 00:51:46 -0500

    Feature: API bash request added

---
+++ API_Request.sh
@@ -0,0 +1,30 @@
+#################################
+# Author: Luis Copete			#
+# Role: Data Engineer			#
+# Linkedin: in/luiscopete		#
+#################################
+
+
+# API URL
+url="https://www.simem.co/backend-files/api/PublicData?startdate=$2&enddate=$3&datasetId=$1"
+
+# Request API and host the response in a variable
+response=$(curl -s "$url")
+
+# check response
+if [ $? -ne 0 ]; then
+    echo "Error during the API request."
+    exit 1
+fi
+
+# check if the response if empty
+if [ -z "$response" ]; then
+    echo "The response is empty."
+    exit 1
+fi
+
+data=$(echo "$response" | jq '.')
+
+# save the results in a file
+echo "The extraction for dataset $1 in the dates range $2 / $3 is succeesfully at $(date)"
+echo "$data" > $1/$1.json
