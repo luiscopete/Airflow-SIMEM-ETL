81c2bc08870728c097f91cb93c068b3619cc6848
Luis Copete <alonsocopete@outlook.es>
2024-03-05 19:31:26 -0500

    FT: Syntax improvement

--- dags/test.py
+++ dags/test.py
@@ -9,17 +9,17 @@ def api_get_file(dataset_id: str, startdate: str, enddate: str) -> None:
     ''' Run the API_Request.sh script to extract data from the API '''
     script_path = os.path.join(scripts_directory, 'API_Request.sh')
     print(script_path)
-    subprocess.run(['chmod', '+x', script_path], check=True)
+    subprocess.run(['chmod', '+x', script_path], check=True) #change permissions to execute the script
     subprocess.run([script_path, dataset_id, startdate, enddate], check=True) #shell=True)
 
 def transform_json(dataset_id: str, output_file_name: str) -> None:
     ''' Run the transform_json.py script to transform the data '''
-    file_path = os.path.join(current_directory, '..' ,'raw')
-    output_path = os.path.join(current_directory, '..' , 'transformed')
+    file_path = os.path.join(current_directory, '..' ,'raw') # find folder in the parent directory
+    output_path = os.path.join(current_directory, '..' , 'transformed') #folder in the parent directory
     script_path = os.path.join(scripts_directory, 'transform_json.sh')
     print(file_path)
     print(script_path)
-    subprocess.run(['chmod', '+x', script_path], check=True)
+    subprocess.run(['chmod', '+x', script_path], check=True) #change permissions to execute the script
     subprocess.run([script_path, file_path, dataset_id, output_path, output_file_name], check=True)
 
 if __name__ == "__main__":
--- scripts/azure_connection.py
+++ scripts/azure_connection.py
@@ -2,23 +2,24 @@ from azure.identity import DefaultAzureCredential
 from azure.identity import InteractiveBrowserCredential
 from azure.storage.filedatalake import DataLakeServiceClient
 
-credential = InteractiveBrowserCredential()
+#credential = DefaultAzureCredential() #autenticación method
+credential = InteractiveBrowserCredential() #autenticación method
 
-account_url = "https://stgpersonalprj.dfs.core.windows.net/"
-
-file_system_name = "raw"
-file_name = "requirements.txt"
-
-destination_path = "/folder/{}" .format(file_name)  # Puedes especificar la ruta deseada
-
-local_file_path = "./requirements.txt"
-
-service_client = DataLakeServiceClient(account_url=account_url, credential=credential)
-
-file_system_client = service_client.get_file_system_client(file_system=file_system_name)
-
-file_client = file_system_client.get_file_client(destination_path)
-
-with open(local_file_path, "rb") as local_file:
-    file_client.upload_data(local_file, overwrite=True)
-
+def upload_file_to_data_lake(storage_account, destination_folder, local_file_path, file_system, dl_file_name):
+    ''' upload file to data lake
+    Args: 
+        storage_account (str): storage account name
+        destination_path (str): destination path in the data lake
+        local_file_path (str): local file path to upload 
+        dl_file_name (str): file name in the data lake'''
+    account_url = f"https://{storage_account}.dfs.core.windows.net/"
+    destination_path = f"/{destination_folder}/{dl_file_name}"  # Puedes especificar la ruta deseada
+    service_client = DataLakeServiceClient(account_url=account_url, credential=credential)
+    file_system_client = service_client.get_file_system_client(file_system=file_system)
+    file_client = file_system_client.get_file_client(destination_path)
+    try:
+        with open(local_file_path, "rb") as local_file:
+            file_client.upload_data(local_file, overwrite=True)
+        return True
+    except Exception as e:
+        raise e
